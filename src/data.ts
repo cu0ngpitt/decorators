1727e4595b0aaa50a7b85e81198a1169ffc47366
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-31 13:25:19 -0600

    Sample data for iteration

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const RECIPES: any = [
+
+  {
+    id: 1,
+    title: 'Artichoke Spinach Dip',
+    imageUrl: 'https://www.onionringsandthings.com/wp-content/uploads/2012/10/spinach-artichoke-dip-1.jpg',
+    description: 'A blend of spinach, artichokes and cheese served warm with baguette slices.',
+    category: 'appetizer'
+  },
+  {
+    id: 1,
+    title: 'Spinach Walnut Salad',
+    imageUrl: 'https://walnuts.org/wp-content/uploads/2017/09/6a31633d4b1b2d0-CFB8C3DB-72C3-491F-BB4BAAFECC21AFA2-900x600.jpg',
+    description: 'Baby spinached served with grilled onions, chopped apples, avocado, and toasted walnuts.',
+    category: 'salad'
+  },
+  {
+    id: 1,
+    title: 'Chicken Marsala',
+    imageUrl: 'https://www.onceuponachef.com/images/2018/01/Chicken-Marsala-760x533.jpg',
+    description: 'An Italian-American dish made from chicken cutlets, mushrooms, and Marsala wine.',
+    category: 'entree'
+  },
+  {
+    id: 1,
+    title: 'Tiramisu',
+    imageUrl: 'https://vignette.wikia.nocookie.net/italian-foodpasta/images/8/87/Tiramisu.jpg/revision/latest?cb=20141008132139',
+    description: 'Ladyfingers dipped in coffee, layered with mascarpone cream.',
+    category: 'dessert'
+  }
+
+];
